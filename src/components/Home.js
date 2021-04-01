c309f0c97da7e222525ccbed27cd46cf0690db9f
Guilherme Donada <guilhermedonada@gmail.com>
2021-04-01 00:01:59 -0300

    iniciado trabalho com css

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../assets/css/home.css'
+
+const Home = () => {
+  return(
+    <article>
+      <section>
+        <a>aaa</a>
+      </section>
+
+      <section>
+        
+      </section>
+    </article>
+  )
+}
+
+export default Home
